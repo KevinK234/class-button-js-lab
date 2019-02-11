7017c9b4243617a5ee0cc4eb07bc058d479ae27e
Kevin K <kevin.kwarteng@sascholar.org>
2019-02-11 04:42:51 +0000

    I changed it so it every button would click and unclick.

--- buttons.js
+++ buttons.js
@@ -1,5 +1,64 @@
 let popbutton = document.querySelector("#popbutton")
 
 popbutton.addEventListener("click", e => {
-    console.log("pop button clicked!")
+    console.log("pop button click!")
+    if(popbutton.classList.contains("pop")){
+        popbutton.classList.remove("pop")
+        
+    }else{
+        popbutton.classList.add("pop")
+    }
 })
+
+let bigbutton = document.querySelector("#bigbutton")
+
+ bigbutton.addEventListener("click", e => {
+
+    console.log("big button clicked!")
+    if(bigbutton.classList.contains("big")){
+        bigbutton.classList.remove("big")
+        
+    }else{
+        bigbutton.classList.add("big")
+    }
+})
+
+let fancybutton = document.querySelector("#fancybutton")
+
+fancybutton.addEventListener("click", e => {
+    console.log("fancy button clicked!")
+    if(fancybutton.classList.contains("fancy")){
+        fancybutton.classList.remove("fancy")
+        
+    }else{
+        fancybutton.classList.add("fancy")
+    }
+})
+
+let negativebutton = document.querySelector("#negativebutton")
+ 
+ negativebutton.addEventListener("click", e => {
+     console.log("negative button clicked!")
+     if(negativebutton.classList.contains("negative")){
+        negativebutton.classList.remove("negative")
+        
+    }else{
+        negativebutton.classList.add("negative")
+    }
+})
+
+let coolbutton = document.querySelector("#coolbutton")
+
+coolbutton.addEventListener("click", e => {
+    console.log("cool button clicked!")
+    if(coolbutton.classList.contains("cool")){
+    coolbutton.classList.remove("cool")
+        
+    }else{
+        coolbutton.classList.add("cool")
+    }
+    
+})
+
+
+  
